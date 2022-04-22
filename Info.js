71dd2ce6d830dc47cd4fa83dbfb8ee34b95c388f
Alejandra Landa <98127121+aleLanda-rq@users.noreply.github.com>
2022-04-22 15:58:58 -0500

    Create Info.js

---
+++ Info.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+
+export default function Info(){
+    return (
+        <div>
+        <img className="image" src="./mypic"/>
+        <h2 className="name">Alejandra Landa </h2>
+        <p className="job">Frontend Developer</p>
+        <button className="emailbttn">Email Me</button>
+        </div>
+    )
+}
