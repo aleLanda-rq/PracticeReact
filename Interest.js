0945441000890ee400ce5244b94ae19f5936e51c
Alejandra Landa <98127121+aleLanda-rq@users.noreply.github.com>
2022-04-22 15:58:45 -0500

    Create Interest.js

---
+++ Interest.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Interest(){
+    return (
+        <div>
+         <h2 className="interest">Interest</h2>
+         <p className="paraDos">My interest are reading webtoons, creating new projects, and watching dramas on TV.</p>
+        </div>
+    )
+}
