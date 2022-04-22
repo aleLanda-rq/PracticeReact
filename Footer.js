e43b9fee1be7ed73bdb029f77b5501c852dc327b
Alejandra Landa <98127121+aleLanda-rq@users.noreply.github.com>
2022-04-22 15:59:15 -0500

    Create Footer.js

---
+++ Footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Footer(){
+    return (
+        <footer>
+        <button className="github"><img className="githubpic" src="./github.png"/></button>
+        <button className="linkedin"><img className="linkedinpic" src="./linkedin.png"/></button>
+        </footer>
+        
+    )
+}
