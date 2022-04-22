f1ae722368bc03f006037dc5ececa77a97f3f3ca
Alejandra Landa <98127121+aleLanda-rq@users.noreply.github.com>
2022-04-22 15:59:47 -0500

    Create About.js

---
+++ About.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function About(){
+    return (
+        <div>
+       <h2 className="about">About</h2>
+       <p className="para">I'm a sophomore at The University Of The People getting my bachelor's degree in Computer Science. I'm currently looking for roles in Front-End Development, specializing in Javascript/React. As well as, internships as a Full Stack Software Engineer.</p>
+        </div>
+    )
+}
